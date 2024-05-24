0fe80f9216987730e8cf2394e353958fd39f2ad6
Tiago Gadonski Cordeiro <tiagogadonski@hotmail.com>
2024-05-24 10:19:40 -0300

    layout corrections

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+    var toastEl = document.getElementById('liveToast');
+    var toast = new bootstrap.Toast(toastEl);
+    toast.show();
+});
